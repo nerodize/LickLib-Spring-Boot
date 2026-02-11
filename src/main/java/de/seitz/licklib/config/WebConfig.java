a9a9b6a96a39021d70fe23b5779beec534315df1
alex seitz <alexseitz248@gmail.com>
2026-02-11 21:12:41 +0100

    feat: added universal CORS ruleset config

---
+++ src/main/java/de/seitz/licklib/config/WebConfig.java
@@ -0,0 +1,24 @@
+package de.seitz.licklib.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Alle Pfade unter /api
+                        .allowedOrigins("http://localhost:4200") // Deine Angular-URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
